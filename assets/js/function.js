4ac4c7f49285c4766e2eb0b4049b601692bfab78
EZ MICHAEL YUCOR <ezmichaely@gmail.com>
2023-05-17 20:07:21 +0800

    update

--- assets/js/function.js
+++ assets/js/function.js
@@ -12,40 +12,9 @@ function openNextPageOnScrollDown(page) {
     });
 }
 
-
-// function openNextPageOnScrollDown(page) {
-//     // Specify the number of pixels to scroll down to trigger the page transition
-//     const scrollThreshold = 800;
-//     let isScrollingDown = false;
-
-//     // Add an event listener to detect mouse wheel scrolling
-//     window.addEventListener('wheel', (event) => {
-//         if (event.deltaY > 0) {
-//             isScrollingDown = true;
-//         } else {
-//             isScrollingDown = false;
-//         }
-//     });
-
-//     // Add an event listener to detect scroll position
-//     window.addEventListener('scroll', () => {
-//         const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-//         // Check if the user has scrolled down and beyond the threshold
-//         if (isScrollingDown && currentScrollPosition >= scrollThreshold) {
-//             // Perform your desired action, such as redirecting to another page
-//             window.location.href = page;
-//         }
-//     });
-// }
-
-
 function openNextPageOnScrollUp(page) {
-    // Specify the number of pixels to scroll up to trigger the page transition
-    const scrollThreshold = 800;
     let isAtTop = true;
 
-    // Add an event listener to detect mouse wheel scrolling
     window.addEventListener('wheel', (event) => {
         if (isAtTop && event.deltaY < 0) {
             // Perform your desired action, such as redirecting to another page
